7946daf687a1f3fc9c76d3d53f93068feb620c43
SamuelRodrigoHernandez <13766537@alu.murciaeduca.es>
2025-03-11 19:19:47 +0100

    Subimos el repositorio.

--- ed04/src/main/java/org/example/Agenda.java
+++ ed04/src/main/java/org/example/Agenda.java
@@ -4,13 +4,25 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * @autor SamuelRodrigoHernandez.
+ * @version 1.0 11/03/2025.
+ */
 public class Agenda {
     private List<Contacto> contacts; // Lista de Contacto
 
+    /**
+     * Crea una agenda de contactos con una lista de array.
+     */
     public Agenda() {
         this.contacts = new ArrayList<>();
     }
 
+    /**
+     * Añade un nuevo contacto, con una condición d eque si el contacto existe se rompa y si no existe se cree ese nuevo contacto.
+     * @param name Aqui tiene una función en la cual llaman a name para saber si existe ese contacto en la agenda.
+     * @param phone Y phone se añade si la condición lo permite y name no existe en la lista de array de la agenda.
+     */
     public void addContact(String name, String phone) {
         boolean exists = false;
         for (Contacto c : contacts) {
@@ -27,6 +39,9 @@ public class Agenda {
         }
     }
 
+    /**
+     * @param name Se llama a name para eliminar el conatacto.
+     */
     public void removeContact(String name) {
         Iterator<Contacto> it = contacts.iterator();
 
@@ -39,6 +54,11 @@ public class Agenda {
         }
     }
 
+    /**
+     * @param name Si name es igual al nombre que especifiquen llama a la lista de telefonos para obtener su numero.
+     * @param oldPhone Se crea un atributo con el antiguo telefono.
+     * @param newPhone Se cmbia el antiguo telefono llamado anteriormente como index y se añade el nuevo telefono.
+     */
     public void modifyPhoneNumber(String name, String oldPhone, String newPhone) {
         for (Contacto c : contacts) {
             if (c.getName().equalsIgnoreCase(name)) {
@@ -53,6 +73,9 @@ public class Agenda {
         }
     }
 
+    /**
+     * @return Nos devuelve la lista de contactos.
+     */
     public List<Contacto> getContacts() {
         return this.contacts;
     }
--- ed04/src/main/java/org/example/Contacto.java
+++ ed04/src/main/java/org/example/Contacto.java
@@ -3,19 +3,33 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author SamuelRodrigoHernandez
+ * @version 1.0 11/03/2025
+ */
 class Contacto {
     private String name;
     private List<String> phones;
-
+    /**
+     * @param: name Este atributo es el nombre del contacto.
+     * @param: phones Este es un atributo pora una lista de telefonos con array.
+     */
     public Contacto(String name, String phone) {
         this.name = name;
         this.phones = new ArrayList<>();
         this.phones.add(phone);
     }
 
+    /**
+     * @return name devuelve el nombre del contacto.
+     */
     public String getName() {
         return this.name;
     }
+
+    /**
+     * @return devuelce la lista de telefonos.
+     */
 
     public List<String> getPhones() {
         return this.phones;
