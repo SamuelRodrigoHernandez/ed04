583b7a6a190488b277fa84d115951984b840a464
SamuelRodrigoHernandez <13766537@alu.murciaeduca.es>
2025-03-12 16:50:25 +0100

    Commit Persona

--- ed04/src/main/java/org/example/Persona.java
+++ ed04/src/main/java/org/example/Persona.java
@@ -7,14 +7,14 @@ import java.util.List;
  * @author SamuelRodrigoHernandez
  * @version 1.0 11/03/2025
  */
-class Contacto {
+class Persona {
     private String name;
     private List<String> phones;
     /**
      * @param: name Este atributo es el nombre del contacto.
      * @param: phones Este es un atributo pora una lista de telefonos con array.
      */
-    public Contacto(String name, String phone) {
+    public Persona(String name, String phone) {
         this.name = name;
         this.phones = new ArrayList<>();
         this.phones.add(phone);
